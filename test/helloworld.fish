17e551036556762c56884ac6c90deb4399de1e23
rai <mj82xx@gmail.com>
2023-03-06 19:01:50 +0900

    check all test has passed

--- test/helloworld.fish
+++ test/helloworld.fish
@@ -19,3 +19,13 @@ set actual (helloworld)
 assert "$expected" "$actual"
 
 echo "===  END TESTS  ==="
+
+if $is_success
+    echo ""
+    echo "All tests passed!"
+    exit 0
+else
+    echo ""
+    echo "Some tests failed!"
+    exit 1
+end
