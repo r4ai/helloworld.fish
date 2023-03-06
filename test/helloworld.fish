0b49f1390884dabf58683a19459e5acb13bcfe27
rai <mj82xx@gmail.com>
2023-03-06 18:56:00 +0900

    assert functions

--- test/helloworld.fish
+++ test/helloworld.fish
@@ -1,2 +1,13 @@
 #!/usr/bin/env fish
 set is_success true
+
+function assert
+    set expected $argv[1]
+    set actual $argv[2]
+    if [ "$expected" != "$actual" ]
+        echo "ERROR: expected '$expected', got '$actual'"
+    else
+        echo "SUCCESS: expected '$expected', got '$actual'"
+        set is_success true
+    end
+end
