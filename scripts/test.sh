43f74e24d75fb710010b18b713d963339265dc5e
rai <mj82xx@gmail.com>
2023-03-06 19:44:16 +0900

    useful scripts

---
+++ scripts/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+
+eval (git rev-parse --show-toplevel 2>/dev/null)/test/helloworld.fish
---
+++ scripts/update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+
+fisher update (git rev-parse --show-toplevel 2>/dev/null)
